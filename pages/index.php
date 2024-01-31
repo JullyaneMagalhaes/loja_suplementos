edc23ca98731090b96dd0fb9f0cfc5127d980804
Jullyane Magalhães <jullyaneflmdigital@gmail.com>
2024-01-31 20:46:27 -0300

    inicio do suplementos IA

---
+++ pages/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<?php
+	include 'header.php';
+	?>
+</head>
+
+<body>
+	<div class="wrapper">
+		<?php
+		include 'menu.php';
+		?>
+		<div class="main">
+			<?php
+			include 'corpo.php';
+			?>
+			<footer class="footer">
+				<?php
+				include 'footer.php';
+				?>
+			</footer>
+		</div>
+	</div>
+
+	<script src="js/app.js"></script>
+
+</body>
+
+</html>
